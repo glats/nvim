811f62384307f7b9ab469f62e4dd3f32f2e7ff9f
jcuzmar <jcuzmar@falabella.cl>
2026-01-19 16:14:11 -0300

    Document repo conventions and disable env autoformat

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("user_env_noformat", { clear = true }),
+  pattern = { ".env", ".env.*", "*.env", "*.env.*" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+  desc = "Disable autoformat for env files",
+})
